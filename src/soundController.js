8d6b3d4579bba1a0bb7541703ec4e7a7e6d97ce2
Cedric Stoquer <cstoquer@gmail.com>
2018-04-29 23:26:28 +0900

    cleanup

--- src/soundController.js
+++ src/soundController.js
@@ -9,70 +9,20 @@ exports.getSound = function () {
 		exports.sound = new AudioManager({
 			path: 'resources/sounds',
 			files: {
-				pirateSea: {
-					path: 'music',
-					volume: 0.5,
-					background: true,
-					loop: true
-				},
-				dubesque: {
-					path: 'music',
-					volume: 0.6,
-					background: true,
-					loop: true
-				},
-				win: {
-					path: 'music',
-					volume: 0.5,
-					background: true,
-					loop: false
-				},
-				loose: {
-					path: 'music',
-					volume: 0.5,
-					background: true,
-					loop: false
-				},
-				swap: {
-					path: 'effect',
-					background: false
-				},
-				fall: {
-					path: 'effect',
-					background: false
-				},
-				combo_x2: {
-					path: 'effect',
-					background: false
-				},
-				combo_x3: {
-					path: 'effect',
-					background: false
-				},
-				combo_x4: {
-					path: 'effect',
-					background: false
-				},
-				combo_x5: {
-					path: 'effect',
-					background: false
-				},
-				combo_x6: {
-					path: 'effect',
-					background: false
-				},
-				explode: {
-					path: 'effect',
-					background: false
-				},
-				destroy: {
-					path: 'effect',
-					background: false
-				},
-				warning: {
-					path: 'effect',
-					background: false
-				}
+				pirateSea: { path: 'music', volume: 0.5, background: true, loop: true },
+				dubesque: { path: 'music',  volume: 0.6, background: true, loop: true },
+				win:      { path: 'music',  volume: 0.5, background: true, loop: false },
+				loose:    { path: 'music',  volume: 0.5, background: true, loop: false },
+				swap:     { path: 'effect', background: false },
+				fall:     { path: 'effect', background: false },
+				combo_x2: { path: 'effect', background: false },
+				combo_x3: { path: 'effect', background: false },
+				combo_x4: { path: 'effect', background: false },
+				combo_x5: { path: 'effect', background: false },
+				combo_x6: { path: 'effect', background: false },
+				explode:  { path: 'effect', background: false },
+				destroy:  { path: 'effect', background: false },
+				warning:  { path: 'effect', background: false }
 			}
 		});
 	}
